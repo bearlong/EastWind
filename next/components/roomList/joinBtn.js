a56a5861153bc27a35109cfb970663ed95c825bc
lapoChen <d123469@gmail.com>
2024-08-10 18:06:17 +0800

    0810 登入入口完成 列表完成

---
+++ next/components/roomList/joinBtn.js
@@ -0,0 +1,12 @@
+export default function JoinBTN() {
+  return (
+    <div className="d-flex join-gw">
+      <div className="partyHolder-gw active-gw">
+        <a href="#">訂桌/開團</a>
+      </div>
+      <div className="partyPlayer-gw">
+        <a href="#">參團</a>
+      </div>
+    </div>
+  )
+}
