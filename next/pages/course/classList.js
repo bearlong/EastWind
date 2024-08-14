99be960f7417046ddad30ba2c3e8b06685b89009
au <20yzwang@gmail.com>
2024-08-14 15:12:02 +0800

    sty

--- next/pages/course/classList.js
+++ next/pages/course/classList.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import axios from 'axios';
 import Image from 'next/image'
 import Link from 'next/link'
+import styles from '@/styles/aa/classList.scss'
 
 export default function ClassList() {
   const router = useRouter()
