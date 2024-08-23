f2754c827bdb7b94832a641748f418fb448fc352
au <20yzwang@gmail.com>
2024-08-23 11:47:35 +0800

    126

--- next/pages/course/classDetail.js
+++ next/pages/course/classDetail.js
@@ -73,14 +73,14 @@ export default function ClassDetail() {
             </div>
           </div>
         </div>
-        <div className="texth2detail2-aa">
-          <div className="texth2detail21-aa">
+        <div className={styles['texth2detail2-aa']}>
+          <div className={styles['texth2detail21-aa']}>
             <h2>課程內容</h2>
           </div>
         </div>
-        <div className="detailpic-aa">
-          <div className="detailpic1-aa">
-            <div className="depic1-aa">
+        <div className={styles['detailpic-aa']}>
+          <div className={styles['detailpic1-aa']}>
+            <div className={styles['depic1-aa']}>
               <img
                 src="https://hahow-production.imgix.net/5fb4fc22563bc0262f9fb105?w=1000&sat=0&auto=format&s=f7cb3bd23dc48b1089edb34423906993"
                 alt=""
@@ -88,28 +88,28 @@ export default function ClassDetail() {
             </div>
           </div>
         </div>
-        <div className="detextgroup-aa">
-          <div className="texth2detail3-aa">
-            <div className="texth2detail31-aa">
+        <div className={styles['detextgroup-aa']}>
+          <div className={styles['texth2detail3-aa']}>
+            <div className={styles['texth2detail31-aa']}>
               <h4>想學程式？推薦你從 麻將 開始！</h4>
             </div>
           </div>
-          <div className="texth2detail4-aa">
+          <div className={styles['texth2detail4-aa']}>
             <h6>
               程式語言的學習，首重邏輯思考能力，面對未來快速變化的社會，若能培養運算思維（Computational
               Thinking）結合工程的務實與效率、數理方面的抽象邏輯思考，將能更有效解決複雜的問題。
             </h6>
           </div>
-          <div className="texth2detail4-aa">
+          <div className={styles['texth2detail4-aa']}>
             <h6>
               程式語言的學習，首重邏輯思考能力，面對未來快速變化的社會，若能培養運算思維（Computational
               Thinking）結合工程的務實與效率、數理方面的抽象邏輯思考，將能更有效解決複雜的問題。
             </h6>
           </div>
         </div>
-        <div className="detailpic-aa">
-          <div className="detailpic1-aa">
-            <div className="depic1-aa">
+        <div className={styles['detailpic-aa']}>
+          <div className={styles['detailpic1-aa']}>
+            <div className={styles['depic1-aa']}>
               <img
                 src="https://hahow-production.imgix.net/5fb4fc22563bc0262f9fb105?w=1000&sat=0&auto=format&s=f7cb3bd23dc48b1089edb34423906993"
                 alt=""
@@ -117,13 +117,13 @@ export default function ClassDetail() {
             </div>
           </div>
         </div>
-        <div className="detextgroup-aa" style={{ paddingBottom: '3rem' }}>
-          <div className="texth2detail3-aa">
-            <div className="texth2detail31-aa">
+        <div className={styles['detextgroup-aa']}>
+          <div className={styles['texth2detail3-aa']}>
+            <div className={styles['texth2detail31-aa']}>
               <h4>想學程式？推薦你從 麻將 開始！</h4>
             </div>
           </div>
-          <div className="texth2detail4-aa">
+          <div className={styles['texth2detail4-aa']}>
             <h6>
               程式語言的學習，首重邏輯思考能力，面對未來快速變化的社會，若能培養運算思維（Computational
               Thinking）結合工程的務實與效率、數理方面的抽象邏輯思考，將能更有效解決複雜的問題。
