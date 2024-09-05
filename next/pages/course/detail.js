c9cc2ad701d0a6cae052cf9a10f83e52ad282b8a
au <20yzwang@gmail.com>
2024-09-05 18:20:17 +0800

    905sty

--- next/pages/course/detail.js
+++ next/pages/course/detail.js
@@ -202,8 +202,6 @@ export default function Detail() {
               </Link>
               &gt;
               <CategoryLink contentData={courses} />
-              {/* &gt;
-              <span> 初級</span> */}
             </h6>
           </div>
           <div className={styles['desec1box-aa']}>
@@ -222,7 +220,7 @@ export default function Detail() {
               </div>
               <div className={styles['detextright3-aa']}>
                 <div className={styles['textrighth51-aa']}>
-                  {/* <h5>徐乃麟</h5> */}
+                  <h5>上架日期: {courses.on_datetime}</h5>
                 </div>
                 <button
                   className={styles['fav1-aa']}
